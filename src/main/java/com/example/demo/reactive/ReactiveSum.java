b49f96bfaa0d57694ee38a7741c3da06756b5d92
Hemant Ghaydar <hemantachi@gmail.com>
2024-07-21 14:27:15 +0530

    changes

--- src/main/java/com/example/demo/reactive/ReactiveSum.java
+++ src/main/java/com/example/demo/reactive/ReactiveSum.java
@@ -15,18 +15,19 @@ import rx.observables.ConnectableObservable;
 
 public class ReactiveSum {
 
-	public static void main(String args[]) {
-		ConnectableObservable<String> input = from(System.in);
-		Observable<Double> a = varStream("a", input);
-		Observable<Double> b = varStream("b", input);
-		ReactiveSum sum = new ReactiveSum(a, b);
-		input.connect();
-	}
+    public static void main(String args[]) {
+        ConnectableObservable<String> input = from(System.in);
+        Observable<Double> a = varStream("a", input);
+        Observable<Double> b = varStream("b", input);
+        //ReactiveSum sum = new ReactiveSum(a, b);
+        input.connect();
+    }
 
-	static ConnectableObservable<String> from(final InputStream stream) {
-		return from(new BufferedReader(new InputStreamReader(stream)));
-		// (1)
-	}
+    static ConnectableObservable<String> from(final InputStream stream) {
+//        return from(new BufferedReader(new InputStreamReader(stream)));
+        return null;
+        // (1)
+    }
 
 //	static ConnectableObservable<String> from(final BufferedReader
 //			reader) {
@@ -57,22 +58,23 @@ public class ReactiveSum {
 //
 //}}
 
-public static Observable<Double> varStream(final String varName, Observable<String> input) {
-     final Pattern pattern = Pattern.compile("\\^s*" + varName +"\\s*[:|=]\\s*(-?\\d+\\.?\\d*)$"); // (1)
-     return input.map(new Func1<String, Matcher>() {
-                public Matcher call(String str) {
-                     return pattern.matcher(str); // (2)
-                }
-            }).filter(new Func1<Matcher, Boolean>() {
-  
-            	 public Boolean call(Matcher matcher) {
-                          return matcher.matches() && matcher.group(1) != null; //
-                }
-          })
-           .map(new Func1<Matcher, Double>() {
-public Double call(Matcher matcher) {
-return Double.parseDouble(matcher.group(1)); // (4)
+    public static Observable<Double> varStream(final String varName, Observable<String> input) {
+        final Pattern pattern = Pattern.compile("\\^s*" + varName + "\\s*[:|=]\\s*(-?\\d+\\.?\\d*)$"); // (1)
+        return input.map(new Func1<String, Matcher>() {
+                    public Matcher call(String str) {
+                        return pattern.matcher(str); // (2)
+                    }
+                }).filter(new Func1<Matcher, Boolean>() {
 
-}
-});
+                    public Boolean call(Matcher matcher) {
+                        return matcher.matches() && matcher.group(1) != null; //
+                    }
+                })
+                .map(new Func1<Matcher, Double>() {
+                    public Double call(Matcher matcher) {
+                        return Double.parseDouble(matcher.group(1)); // (4)
+
+                    }
+                });
+    }
 }
