404fd1c3dbf55b764d8b6740db2522e586915239
hemant <hemant_ghaydar@apple.com>
2024-10-14 17:11:33 +0530

    minor changes

---
+++ src/main/java/com/example/demo/stream/reactive/ReactiveProgramming.java
@@ -0,0 +1,30 @@
+package com.example.demo.stream.reactive;
+
+import rx.Observable;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+public class ReactiveProgramming {
+
+    public static void main(String[] args) {
+        // older way
+        List<String> list = Arrays.asList("A", "B", "C");
+        Iterator<String> listIterator = list.iterator();
+        while (listIterator.hasNext()) {
+            System.out.println(listIterator.next());
+        }
+        System.out.println("\n\n");
+        // new way the producer 'pushes' the values as notifications to the consumer.
+        List<String> newList = Arrays.asList("One", "Two", "Three", "Four", "Five");
+        Observable<String> observable = Observable.from(newList);
+
+        observable.subscribe(r -> {
+            System.out.println(r);
+        }, e -> {
+            System.out.println("Error Occurred...");
+        }, () -> System.out.println("Completed..."));
+
+    }
+}
