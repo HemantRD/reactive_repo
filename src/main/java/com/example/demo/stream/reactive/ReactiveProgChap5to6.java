78aa547352009f89fe90e8f9d698735e714b6b88
hemant <hemant_ghaydar@apple.com>
2024-11-11 16:19:56 +0530

    minor changes

--- src/main/java/com/example/demo/stream/reactive/ReactiveProgChap5to6.java
+++ src/main/java/com/example/demo/stream/reactive/ReactiveProgChap5to6.java
@@ -5,12 +5,28 @@ import rx.Observable;
 import java.util.Arrays;
 import java.util.concurrent.TimeUnit;
 
-import static com.example.demo.stream.reactive.ReactiveProgChap1To4.blockingSubscribePrint;
-import static com.example.demo.stream.reactive.ReactiveProgChap1To4.subscribePrint;
+import static com.example.demo.stream.reactive.ReactiveProgChap1To4.*;
 
 public class ReactiveProgChap5to6 {
 
     public static void main(String[] args) {
+        Observable<String> greetings = Observable.just("Hello", "Hi", "Howdy",
+                        "Zdravei", "Yo", "Good to see ya")
+                .zipWith(Observable.interval(1L, TimeUnit.SECONDS), ReactiveProgChap5to6::onlyFirstArg);
+        Observable<String> names = Observable.just("Meddle", "Tanya", "Dali", "Joshua")
+                .zipWith(Observable.interval(1500L, TimeUnit.MILLISECONDS),
+                        ReactiveProgChap5to6::onlyFirstArg);
+        Observable<String> punctuations = Observable.just(".", "?", "!", "!!!", "...").zipWith(
+                Observable.interval(1100L, TimeUnit.MILLISECONDS),
+                ReactiveProgChap5to6::onlyFirstArg);
+
+        //startWith operator similar to concat
+        Observable<String> concat = punctuations.startWith(names).startWith(greetings);
+        blockingSubscribePrint(concat, "startsWith");
+
+    }
+
+    public static void main2(String[] args) {
         //combineLatest example
         Observable<String> greetings = Observable.just("Hello", "Hi", "Howdy",
                         "Zdravei", "Yo", "Good to see ya")
@@ -37,6 +53,10 @@ public class ReactiveProgChap5to6 {
         Observable<String> mergeDelayError = Observable.mergeDelayError(greetings, names, punctuations);
         blockingSubscribePrint(merged, "mergeDelayError");
 
+        System.out.println("\n\n\n");
+        //contact operator
+        Observable<String> concat = Observable.concat(greetings, names, punctuations);
+        blockingSubscribePrint(concat, "concat Operator");
     }
 
     public static <T, R> T onlyFirstArg(T arg1, R arg2) {
