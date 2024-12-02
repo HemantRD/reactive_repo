e364ab4e1d8a36422cdc19ba1e3d8c6d47e56ad0
hemant <hemant_ghaydar@apple.com>
2024-12-02 18:00:16 +0530

    minor changes

--- src/main/java/com/example/demo/stream/reactive/ReactiveProgChap7to8.java
+++ src/main/java/com/example/demo/stream/reactive/ReactiveProgChap7to8.java
@@ -8,6 +8,7 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import rx.Observable;
+import rx.Subscriber;
 import rx.apache.http.ObservableHttp;
 import rx.apache.http.ObservableHttpResponse;
 import rx.functions.Action1;
@@ -21,12 +22,119 @@ import java.nio.charset.StandardCharsets;
 import java.util.*;
 import java.util.concurrent.TimeUnit;
 
+import static com.example.demo.stream.reactive.ReactiveProgChap1To4.subscribePrint;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class ReactiveProgChap7to8 {
-
     public static void main(String[] args) {
+        // creating custom operator
+        Observable<Pair<Long, String>> observable = Observable
+                .just("a", "b", "c", "d", "e")
+                .lift(new Indexed<String>());
+        subscribePrint(observable, "Custom Operator");
+    }
+
+    public static class Pair<L, R> {
+        final L left;
+        final R right;
+
+        public Pair(L left, R right) {
+            this.left = left;
+            this.right = right;
+        }
+
+        public L getLeft() {
+            return left;
+        }
+
+        public R getRight() {
+            return right;
+        }
+
+        @Override
+        public int hashCode() {
+            final int prime = 31;
+            int result = 1;
+            result = prime * result + ((left == null) ? 0 : left.hashCode());
+            result = prime * result + ((right == null) ? 0 : right.hashCode());
+            return result;
+        }
+
+        @SuppressWarnings("rawtypes")
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) {
+                return true;
+            }
+            if (obj == null) {
+                return false;
+            }
+            if (!(obj instanceof Pair)) {
+                return false;
+            }
+            Pair other = (Pair) obj;
+            if (left == null) {
+                if (other.left != null) {
+                    return false;
+                }
+            } else if (!left.equals(other.left)) {
+                return false;
+            }
+            if (right == null) {
+                if (other.right != null) {
+                    return false;
+                }
+            } else if (!right.equals(other.right)) {
+                return false;
+            }
+            return true;
+        }
+
+        @Override
+        public String toString() {
+            return "Pair{" +
+                    "left=" + left +
+                    ", right=" + right +
+                    '}';
+        }
+    }
+
+    public static class Indexed<T> implements Observable.Operator<Pair<Long, T>, T> {
+        private final long initialIndex;
+
+        public Indexed() {
+            this(0L);
+        }
+
+        public Indexed(long initial) {
+            this.initialIndex = initial;
+        }
+
+        @Override
+        public Subscriber<? super T> call(Subscriber<? super Pair<Long, T>> subscriber) {
+            return new Subscriber<T>(subscriber) {
+                private long index = initialIndex;
+
+                @Override
+                public void onCompleted() {
+                    subscriber.onCompleted();
+                }
+
+                @Override
+                public void onError(Throwable throwable) {
+                    subscriber.onError(throwable);
+                }
+
+                @Override
+                public void onNext(T t) {
+                    subscriber.onNext(new Pair<>(index++, t));
+                }
+            };
+        }
+    }
+
+    public static void main2(String[] args) {
         //cache the response example not working
         String url = "https://api.github.com/orgs/ReactiveX/repos";
         Observable<ObservableHttpResponse> response = request(url);
