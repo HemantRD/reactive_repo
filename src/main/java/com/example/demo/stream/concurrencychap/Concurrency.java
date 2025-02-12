9ae5b5fd43874dba111b44ecf672cd18e8d6ecde
hemant <hemant_ghaydar@apple.com>
2025-02-12 09:24:22 +0530

    minor changes

--- src/main/java/com/example/demo/stream/concurrencychap/Concurrency.java
+++ src/main/java/com/example/demo/stream/concurrencychap/Concurrency.java
@@ -3,13 +3,68 @@ package com.example.demo.stream.concurrencychap;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class Concurrency {
 
+    static String[] dogs1 = {"boi", "clover", "charis"};
+    static String[] dogs2 = {"aiko", "zooey", "biscuit"};
+
+    static class ProcessDogs implements Runnable {
+        String dogs[];
+        CyclicBarrier barrier;
+
+        ProcessDogs(String[] d, CyclicBarrier barrier) {
+            dogs = d;
+            this.barrier = barrier;
+        }
+
+        public void run() {
+            for (int i = 0; i < dogs.length; i++) {
+                String dogName = dogs[i];
+                String newDogName = dogName.substring(0, 1).toUpperCase() + dogName.substring(1);
+                dogs[i] = newDogName;
+            }
+            try {
+                if (Thread.currentThread().getName().equalsIgnoreCase("A")) {
+                    Thread.sleep(5000);
+                }
+                if (Thread.currentThread().getName().equalsIgnoreCase("B")) {
+                    Thread.sleep(10000);
+                }
+                barrier.await();
+            } catch (InterruptedException | BrokenBarrierException e) {
+                e.printStackTrace();
+            }
+            System.out.println(Thread.currentThread().getName() + " is done!");
+        }
+    }
+
     public static void main(String[] args) throws Exception {
+        // CyclicBarrier example
+        List<String> result = new ArrayList<>();
+        //number of threads that will wait at the barrier and a Runnable that will
+        //be run by the last thread to reach the barrier
+        CyclicBarrier barrier = new CyclicBarrier(2, () -> {
+            for (int i = 0; i < dogs1.length; i++) {
+                result.add(dogs1[i]);
+            }
+            for (int i = 0; i < dogs2.length; i++) {
+                result.add(dogs2[i]);
+            }
+            System.out.println(Thread.currentThread().getName() + " Result:" + result);
+        });
 
+        Thread t1 = new Thread(new ProcessDogs(dogs1, barrier));
+        t1.setName("A");
+        Thread t2 = new Thread(new ProcessDogs(dogs2, barrier));
+        t2.setName("B");
 
+        t1.start();
+        t2.start();
+        System.out.println("Main Thread is done");
     }
 
     static class ArrayListRunnable implements Runnable {
