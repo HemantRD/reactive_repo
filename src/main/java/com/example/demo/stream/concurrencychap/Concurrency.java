70b7ab321f8c347469687b8ceb94ffc4756c054c
hemant <hemant_ghaydar@apple.com>
2025-02-19 11:14:35 +0530

    minor changes

--- src/main/java/com/example/demo/stream/concurrencychap/Concurrency.java
+++ src/main/java/com/example/demo/stream/concurrencychap/Concurrency.java
@@ -10,6 +10,58 @@ import java.util.concurrent.locks.ReentrantReadWriteLock;
 public class Concurrency {
 
     public static void main(String[] args) {
+        // Parallel Fork/Join example return value
+        int[] data = new int[10000000];
+        ForkJoinPool forkJoinPool = new ForkJoinPool(2);
+        RandomInitRecursiveAction action = new RandomInitRecursiveAction(data, 0, data.length);
+        forkJoinPool.invoke(action);
+
+        FindMaxPositionRecursiveTask task = new FindMaxPositionRecursiveTask(data, 0, data.length);
+        Integer position = forkJoinPool.invoke(task);
+        System.out.println("Position :" + position + ", value :" + data[position]);
+    }
+
+    static class FindMaxPositionRecursiveTask extends RecursiveTask<Integer> {
+        private static final int THRESHOLD = 10000;
+        private int[] data;
+        private int start;
+        private int end;
+
+        public FindMaxPositionRecursiveTask(int[] data, int start, int end) {
+            this.data = data;
+            this.start = start;
+            this.end = end;
+        }
+
+        @Override
+        protected Integer compute() {
+            if (end - start <= THRESHOLD) {
+                int position = 0;
+                for (int i = start; i < end; i++) {
+                    if (data[i] > data[position]) {
+                        position = i;
+                    }
+                }
+                return position;
+            } else {
+                int halfway = ((end - start) / 2) + start;
+                FindMaxPositionRecursiveTask t1 = new FindMaxPositionRecursiveTask(data, start, halfway);
+                t1.fork();
+                FindMaxPositionRecursiveTask t2 = new FindMaxPositionRecursiveTask(data, halfway, end);
+                int position2 = t2.compute();
+                int position1 = t1.join();
+                if (data[position1] > data[position2]) {
+                    return position1;
+                } else if (data[position2] > data[position1]) {
+                    return position2;
+                } else {
+                    return position1 < position2 ? position1 : position2;
+                }
+            }
+        }
+    }
+
+    public static void main9(String[] args) {
         // Parallel Fork/Join example
         int[] data = new int[10000000];
         ForkJoinPool forkJoinPool = new ForkJoinPool(2);
@@ -34,7 +86,6 @@ public class Concurrency {
             if (end - start <= THRESHOLD) {
                 for (int i = start; i < end; i++) {
                     data[i] = ThreadLocalRandom.current().nextInt();
-                    System.out.println(Thread.currentThread().getName());
                 }
             } else {
                 int halfway = ((end - start) / 2) + start;
