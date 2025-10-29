28d74d7f476bca2b2390cd5990b6936904a37cad
Hemant <hemant_ghaydar@apple.com>
2025-10-29 13:28:07 +0530

    minor changes

--- src/main/java/com/example/demo/spring/chap2/HelloWorld.java
+++ src/main/java/com/example/demo/spring/chap2/HelloWorld.java
@@ -1,7 +1,18 @@
 package com.example.demo.spring.chap2;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class HelloWorld {
-    public static void main(String... args) {
+
+    public static void main(String[] args) {
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("spring/app-context.xml");
+        MessageRenderer mr = ctx.getBean("renderer", MessageRenderer.class);
+        
+        mr.render();
+    }
+
+    public static void main3(String... args) {
         MessageRenderer mr =
                 MessageSupportFactory.getInstance().getMessageRenderer();
         MessageProvider mp =
