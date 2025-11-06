325fcd61b8d8c5f17e97e28ebd75b380255c9fa5
Hemant <hemant_ghaydar@apple.com>
2025-11-06 08:56:45 +0530

    minor changes

--- src/main/java/com/example/demo/stream/streamchap/Reading.java
+++ src/main/java/com/example/demo/stream/streamchap/Reading.java
@@ -1,12 +1,12 @@
 package com.example.demo.stream.streamchap;
 
-class Reading {
+public class Reading {
     int year;
     int month;
     int day;
     double value;
 
-    Reading(int year, int month, int day, double value) {
+    public Reading(int year, int month, int day, double value) {
         this.year = year;
         this.month = month;
         this.day = day;
